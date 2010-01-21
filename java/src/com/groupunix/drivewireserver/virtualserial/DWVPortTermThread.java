0967e3db37982da93642e5677c436273747090a2
aaronwolfe <aaronwolfe>
2010-01-21 01:32:48 +0000

    better handling of term in use situation

--- java/src/com/groupunix/drivewireserver/virtualserial/DWVPortTermThread.java
+++ java/src/com/groupunix/drivewireserver/virtualserial/DWVPortTermThread.java
@@ -21,6 +21,9 @@ public class DWVPortTermThread implements Runnable
 	private static final int MODE_TERM = 3;
 	private static final int BACKLOG = 0;
 	
+	private Thread connthread;
+	private int conno;
+	
 	public DWVPortTermThread(int tcpport)
 	{
 		logger.debug("init term device thread on port "+ tcpport);	
@@ -81,19 +84,31 @@ public class DWVPortTermThread implements Runnable
 			
 			logger.info("new connection from " + skt.getInetAddress().getHostAddress());
 			
-			// pass through till connection is lost
-			int conno = DWVPortListenerPool.addConn(skt,MODE_TERM);
-			Thread connthread = new Thread(new DWVPortTCPServerThread(TERM_PORT, conno));
-			connthread.start();
-			try
-			{
-				connthread.join();
-			} 
-			catch (InterruptedException e)
-			{
-				logger.warn("interrupted while waiting on server thread: " + e.getMessage());
+			if (this.connthread != null)
+			{	
+				if (this.connthread.isAlive())
+				{
+					// no room at the inn
+					logger.debug("term connection already in use");
+					try
+					{
+						skt.getOutputStream().write(("The term device is already connected to a session (from " + DWVPortListenerPool.getConn(conno).getInetAddress().getHostName() + ")\r\n" ).getBytes());
+						skt.close();
+					} 
+					catch (IOException e)
+					{
+						logger.debug("io error closing socket: " + e.getMessage());
+					}
+				}
+				else
+				{
+					startConn(skt);
+				}
 			}
-			
+			else
+			{
+				startConn(skt);
+			}
 		}
 			
 		if (srvr != null)
@@ -110,5 +125,48 @@ public class DWVPortTermThread implements Runnable
 		
 		logger.debug("exiting");
 	}
+
+	private void startConn(Socket skt)
+	{
+		// do telnet init stuff
+		byte[] buf = new byte[9];
+	
+		buf[0] = (byte) 255;
+		buf[1] = (byte) 251;
+		buf[2] = (byte) 1;
+		buf[3] = (byte) 255;
+		buf[4] = (byte) 251;
+		buf[5] = (byte) 3;
+		buf[6] = (byte) 255;
+		buf[7] = (byte) 253;
+		buf[8] = (byte) 243;
+	
+	
+		try
+		{
+			skt.getOutputStream().write(buf, 0, 9);
+			for (int i = 0; i<9; i++)
+			{
+				skt.getInputStream().read();
+			}
+		} catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+		
+		
+		
+		// pass through till connection is lost
+		conno = DWVPortListenerPool.addConn(skt,MODE_TERM);
+		connthread = new Thread(new DWVPortTCPServerThread(TERM_PORT, conno));
+		connthread.start();
+	
+		
+	}
 		
 }
+
+
+
