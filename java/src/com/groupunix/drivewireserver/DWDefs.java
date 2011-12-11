38f71cbb66177af6d64de12ee30156fece218a5c
aaronwolfe <aaronwolfe>
2011-12-11 07:04:03 +0000

    cleaned up

--- java/src/com/groupunix/drivewireserver/DWDefs.java
+++ java/src/com/groupunix/drivewireserver/DWDefs.java
@@ -8,40 +8,59 @@ public class DWDefs
 	
 	// DW protocol op codes
 	
-	public static final byte OP_NOP = 0;
+	public static final byte OP_NOP 			= (byte) 0;		// 0x00
+	public static final byte OP_NAMEOBJ_MOUNT 	= (byte) 1;		// 0x01
+	public static final byte OP_NAMEOBJ_CREATE 	= (byte) 2;		// 0x02
+																// 0x03 - 0x0F reserved for named object future use
+	public static final byte OP_TIME 			= (byte) 35;	// 0x23 #
+	public static final byte OP_AARON			= (byte) 65;	// 0x41 A
+	public static final byte OP_WIREBUG_MODE	= (byte) 66;	// 0x42 B	
+	public static final byte OP_SERREAD 		= (byte) 67;	// 0x43 C
+	public static final byte OP_SERGETSTAT 		= (byte) 68;	// 0x44 D
+	public static final byte OP_SERINIT 		= (byte) 69;	// 0x45 E
+	public static final byte OP_PRINTFLUSH		= (byte) 70;	// 0x46 F
+	public static final byte OP_GETSTAT 		= (byte) 71;  	// 0x47 G
+	public static final byte OP_INIT			= (byte) 73;	// 0x49 I
+	public static final byte OP_PRINT 			= (byte) 80;	// 0x50 P
+	public static final byte OP_READ 			= (byte) 82;  	// 0x52 R
+	public static final byte OP_SETSTAT 		= (byte) 83;  	// 0x53 S
+	public static final byte OP_TERM 			= (byte) 84;	// 0x54 T
+	public static final byte OP_WRITE  			= (byte) 87;	// 0x57 W
+	public static final byte OP_DWINIT 			= (byte) 90;	// 0x5A Z
+	public static final byte OP_SERREADM 		= (byte) 99;	// 0x63 c
+	public static final byte OP_SERWRITEM		= (byte) 100;	// 0x64 d
+	public static final byte OP_REREAD 			= (byte) 114;	// 0x72 r
+	public static final byte OP_REWRITE 		= (byte) 119;	// 0x77 w
+	public static final byte OP_FASTWRITE_BASE 	= (byte) 128; 	// 0x80	
+	public static final byte OP_FASTWRITE_P1 	= (byte) 129; 	// 0x81
+	public static final byte OP_FASTWRITE_P2 	= (byte) 130; 	// 0x82
+	public static final byte OP_FASTWRITE_P3 	= (byte) 131; 	// 0x83
+	public static final byte OP_FASTWRITE_P4 	= (byte) 132; 	// 0x84
+	public static final byte OP_FASTWRITE_P5 	= (byte) 133; 	// 0x85
+	public static final byte OP_FASTWRITE_P6 	= (byte) 134; 	// 0x86
+	public static final byte OP_FASTWRITE_P7 	= (byte) 135; 	// 0x87
+	public static final byte OP_FASTWRITE_P8 	= (byte) 136; 	// 0x88
+	public static final byte OP_FASTWRITE_P9 	= (byte) 137; 	// 0x89
+	public static final byte OP_FASTWRITE_P10 	= (byte) 138; 	// 0x8A
+	public static final byte OP_FASTWRITE_P11 	= (byte) 139; 	// 0x8B
+	public static final byte OP_FASTWRITE_P12 	= (byte) 140; 	// 0x8C
+	public static final byte OP_FASTWRITE_P13 	= (byte) 141; 	// 0x8D
+	public static final byte OP_FASTWRITE_P14	= (byte) 142; 	// 0x8E
+	public static final byte OP_FASTWRITE_P15 	= (byte) 143; 	// 0x8F
+																// 0x90 - 0x9F reserved for future vserial use
+	public static final byte OP_SERWRITE 		= (byte) 195;	// 0xC3 C+128
+	public static final byte OP_SERSETSTAT 		= (byte) 196;	// 0xC4 D+128
+	public static final byte OP_SERTERM 		= (byte) 197;	// 0xC5 E+128
+	public static final byte OP_READEX  		= (byte) 210; 	// 0xD2 R+128
+	public static final byte OP_RFM 			= (byte) 214;	// 0xD6 V+128
+	public static final byte OP_230K230K 		= (byte) 230;	// 0xE6
+	public static final byte OP_REREADEX 		= (byte) 242;	// 0xF2 r+128
+	public static final byte OP_RESET3 			= (byte) 248;	// 0xF8
+	public static final byte OP_230K115K 		= (byte) 253;	// 0xFD
+	public static final byte OP_RESET2 			= (byte) 254;	// 0xFE
+	public static final byte OP_RESET1 			= (byte) 255;	// 0xFF
 	
-	public static final byte OP_NAMEOBJ_MOUNT = 1;
-	public static final byte OP_NAMEOBJ_CREATE = 2;
 	
-	
-	public static final byte OP_GETSTAT = 'G';
-	public static final byte OP_SETSTAT = 'S';
-	public static final byte OP_READ = 'R';
-	public static final byte OP_READEX = (byte) ('R'+128);
-	public static final byte OP_WRITE = 'W';
-	public static final byte OP_REREAD = 'r';
-	public static final byte OP_REREADEX = (byte) ('r'+128);
-	public static final byte OP_REWRITE = 'w';
-	public static final byte OP_INIT = 'I';
-	public static final byte OP_TERM = 'T';
-	public static final byte OP_TIME = '#';
-	public static final byte OP_RESET3 = (byte) 248;  // my coco gives 248 on reset..?
-	public static final byte OP_RESET2 = (byte) 254;
-	public static final byte OP_RESET1 = (byte) 255;
-	public static final byte OP_DWINIT = (byte) 'Z';
-	public static final byte OP_PRINT = 'P';
-	public static final byte OP_PRINTFLUSH = 'F';
-	public static final byte OP_SERREAD = 'C';  // 67 43
-	public static final byte OP_SERREADM = 'c'; // 99 63
-	public static final byte OP_SERWRITE = (byte) ('C'+128); // 195 C3
-	public static final byte OP_SERSETSTAT = (byte) ('D'+128); // 196 C4
-	public static final byte OP_SERGETSTAT = 'D'; // 68 44
-	public static final byte OP_SERINIT = 'E'; // 69 45
-	public static final byte OP_SERTERM = (byte) ('E'+128); // 197 C5
-	public static final byte OP_RFM = (byte) ('V'+128);
-	
-	public static final byte OP_230K230K = (byte) 230;
-	public static final byte OP_230K115K = (byte) 253;
 	
 	// response codes
 	public static final byte DWERROR_WP = (byte) 0xF2;
@@ -51,12 +70,7 @@ public class DWDefs
 	public static final byte DWERROR_NOTREADY = (byte) 0xF6;
 	public static final byte DWOK = (byte) 0;
 	
-	// input buffer
-	public static final int INPUT_WAIT = 250;
-	
-	// fast writes
-	public static final byte OP_FASTWRITE_BASE = (byte) 128;
-	
+		
 	// util modes
 	public static final int UTILMODE_UNSET = 0;
 	public static final int UTILMODE_DWCMD = 1;
@@ -69,41 +83,43 @@ public class DWDefs
 	
 	
 	// result codes for DW virtual channel API calls - all subject to change until I know of anyone actually using these on client side
-	public static final byte RC_SUCCESS = 0;
-	public static final byte RC_SYNTAX_ERROR = 100;
-	public static final byte RC_INVALID_DRIVE = 101;
-	public static final byte RC_DRIVE_NOT_LOADED = 102;
-	public static final byte RC_DRIVE_ALREADY_LOADED = 103;
-	public static final byte RC_NO_SUCH_DISKSET = 110;
-	public static final byte RC_INVALID_DISK_DEF = 111;
+	public static final byte RC_SUCCESS 						= (byte) 0;
 	
-	public static final byte RC_NET_IO_ERROR = 120;
-	public static final byte RC_NET_UNKNOWN_HOST = 121;
-	public static final byte RC_NET_INVALID_CONNECTION = 122;
-
+	public static final byte RC_SYNTAX_ERROR 					= (byte) 1;
 	
-	public static final byte RC_INVALID_PORT = (byte)140;
-
-	public static final byte RC_MIDI_UNAVAILABLE = (byte)150;
-	public static final byte RC_MIDI_INVALID_DEVICE = (byte)151;
-	public static final byte RC_MIDI_INVALID_DATA = (byte)152;
-	public static final byte RC_MIDI_SOUNDBANK_FAILED = (byte)153;
-	public static final byte RC_MIDI_SOUNDBANK_NOT_SUPPORTED = (byte)154;
-	public static final byte RC_MIDI_INVALID_PROFILE = (byte)155;
-	public static final byte RC_MIDI_ERROR = (byte)156;
+	public static final byte RC_DRIVE_ERROR						= (byte) 100;
+	public static final byte RC_INVALID_DRIVE 					= (byte) 101;
+	public static final byte RC_DRIVE_NOT_LOADED 				= (byte) 102;
+	public static final byte RC_DRIVE_ALREADY_LOADED 			= (byte) 103;
+	public static final byte RC_IMAGE_FORMAT_EXCEPTION 			= (byte) 104;
 	
-	public static final byte RC_SERVER_FILESYSTEM_EXCEPTION = (byte)200;
-	public static final byte RC_SERVER_IO_EXCEPTION = (byte)201;
-	public static final byte RC_SERVER_FILE_NOT_FOUND = (byte)202;
-	public static final byte RC_SERVER_NOT_IMPLEMENTED = (byte)203;
+	public static final byte RC_NO_SUCH_DISKSET 				= (byte) 110;
+	public static final byte RC_INVALID_DISK_DEF 				= (byte) 111;
+	
+	public static final byte RC_NET_ERROR 						= (byte) 120;
+	public static final byte RC_NET_IO_ERROR 					= (byte) 121;
+	public static final byte RC_NET_UNKNOWN_HOST 				= (byte) 122;
+	public static final byte RC_NET_INVALID_CONNECTION 			= (byte) 123;
+	
+	public static final byte RC_INVALID_PORT 					= (byte) 140;
+	public static final byte RC_INVALID_HANDLER 				= (byte) 141;
+	public static final byte RC_CONFIG_KEY_NOT_SET 				= (byte) 142;
+	
+	public static final byte RC_MIDI_ERROR 						= (byte) 150;
+	public static final byte RC_MIDI_UNAVAILABLE 				= (byte) 151;
+	public static final byte RC_MIDI_INVALID_DEVICE 			= (byte) 152;
+	public static final byte RC_MIDI_INVALID_DATA 				= (byte) 153;
+	public static final byte RC_MIDI_SOUNDBANK_FAILED 			= (byte) 154;
+	public static final byte RC_MIDI_SOUNDBANK_NOT_SUPPORTED 	= (byte) 155;
+	public static final byte RC_MIDI_INVALID_PROFILE 			= (byte) 156;
+	
+	public static final byte RC_SERVER_ERROR					= (byte) 200;
+	public static final byte RC_SERVER_FILESYSTEM_EXCEPTION 	= (byte) 201;
+	public static final byte RC_SERVER_IO_EXCEPTION 			= (byte) 202;
+	public static final byte RC_SERVER_FILE_NOT_FOUND 			= (byte) 203;
+	public static final byte RC_SERVER_NOT_IMPLEMENTED 			= (byte) 204;
 
-
-	public static final byte RC_INVALID_HANDLER = (byte) 210;
-
-
-	public static final byte RC_CONFIG_KEY_NOT_SET = (byte)220;
-
-
+	public static final byte RC_FAIL 							= (byte) 255;
 
 	
 	// disk stuff
@@ -112,6 +128,16 @@ public class DWDefs
 	public static final int DISK_MAXSECTORS = 16777215;
 	public static final int DISK_SECTORSIZE = 256;
 
+	public static final int DISK_FORMAT_NONE = 0;
+	public static final int DISK_FORMAT_RAW = 1;
+	public static final int DISK_FORMAT_DMK = 2;
+	public static final int DISK_FORMAT_JVC = 3;
+	public static final int DISK_FORMAT_DK = 4;
+	
+	public static final int DISK_CONSIDER_NO = 0;
+	public static final int DISK_CONSIDER_MAYBE = 1;
+	public static final int DISK_CONSIDER_YES = 2;
+	
 	public static final Boolean DISK_DEFAULT_EXPAND = true;
 	public static final Boolean DISK_DEFAULT_SYNCTO = true;
 	public static final Boolean DISK_DEFAULT_SYNCFROM = true;
@@ -127,6 +153,17 @@ public class DWDefs
 	public static final String HELP_DEFAULT_FILE = "help.xml";
 
 	public static final int DWCMD_DEFAULT_COLS = 80;
+
+	// events
+	public static final byte EVENT_TYPE_SERVERCONFIG = 'C';
+	public static final byte EVENT_TYPE_INSTANCECONFIG = 'I';
+	public static final byte EVENT_TYPE_DISK = 'D';
+	public static final byte EVENT_TYPE_LOG = 'L';
+	public static final byte EVENT_TYPE_MIDI = 'M';
+	public static final byte EVENT_TYPE_NET = 'N';
+	public static final byte EVENT_TYPE_PRINT = 'P';
+
+	
 
 	
 
